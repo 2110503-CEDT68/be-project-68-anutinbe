4915fef98be8884d968f81a203c90294f5c1aa67
Akibara <laanemmone@gmail.com>
2026-02-23 17:25:45 +0700

    Initial code

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db')
+
+//Load env vars
+dotenv.config({path:"./config/config.env"});
+
+//Connect to Database
+// connectDB();
+
+//Route files
+// const auth = require('./routes/auth');
+
+const app = express();
+
+
+//Body parser
+app.use(express.json());
+
+//Cookie parser
+app.use(cookieParser());
+
+app.set('query parser','extended');
+
+//Mount to router
+// app.use('/api/v1/auth',auth);
+
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, console.log('Server running in', process.env.NODE_ENV, 'mode on port', PORT));
+
+process.on('unhandledRejection', (err,promise)=>{
+    console.log(`Error: ${err.message}`);
+
+    server.close(()=>process.exit(1));
+})
